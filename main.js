2fa3acad015f1119d56fe8fec8e5e9fdff5c712f
Qiawox <gv7361299@gmail.com>
2022-05-13 17:40:30 +0400

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
-function Random (){ 
-  const max = 100000;
-  return Math.round(Math.random() * max );
- }
+let phr = ['й','ц','у','к','е','н','г','ш','щ','з','х','ъ','ф','ы','в','а','п','р','о','л','д','ж','э','ю','б','ь','т','и','м','с','ч','я','q','w','e','r','t','y','u','i','o','p','l','k','j','h','g','f','d','s','a','z','x','c','v','b','n','m','1','2','3','4','5','6','7','8','9'];
+  let button = document.getElementById('button');
+  let phrase = document.getElementById('ch1');
 
+button.addEventListener('click', function() {
+  let rand = Math.floor(Math.random() * phr.length);
+  phrase.innerHTML = phr[rand];
+});
   var colorArray = ["#0DC2FF", "#007DE8", "#0E5DFF", "#0C28EB", "#1600FF", "#000000", "red", "yellow", "grey", "pink"];
   var i = 0;
 
@@ -27,7 +30,6 @@ clear.onclick = function() {
   div1.innerHTML = Reset();
 }
  btn.onclick = function() {
-   div.innerHTML = Random();
    div1.innerHTML = changeColor();
  }
 
